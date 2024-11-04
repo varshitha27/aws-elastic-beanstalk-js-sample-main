717aa7d1059c785cace556dc8ef9c32fe6f302b6
varshitha27 <81610663+varshitha27@users.noreply.github.com>
2024-11-04 08:28:27 +0530

    updated app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello All, Happy Learning -- Updated Version'));
+app.get('/', (req, res) => res.send('Hello All, Happy Learning -- Varshitha Buragamanda'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
